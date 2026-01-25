00f1f855a072e1ce094ef9e4ef559ae91153b8cf
Monika <monikanimmana3366@example.com>
2026-01-26 00:00:24 +0530

    Add index.html for GitHub Pages

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%4q8$=l4m4-2nv4+cw9qj-l=l7wm4@3(%nbj=rq=(0^0)v0t12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
