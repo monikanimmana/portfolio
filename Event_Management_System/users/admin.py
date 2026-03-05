2b874b409afe7d7a17267306d73c291ba569e4b0
Monika <monikanimmana3366@gmail.com>
2026-03-05 15:30:10 +0530

    adding views logic

--- Event_Management_System/users/admin.py
+++ Event_Management_System/users/admin.py
@@ -1,12 +1,3 @@
-from django.contrib import admin 
-from django.contrib.auth.admin import UserAdmin
-from .models import *
+from django.contrib import admin
 
 # Register your models here.
-class CustomUserModel(UserAdmin):
-    model = User
-    list_display = ["id","username","email","phone_number","is_staff","is_active"]
-    search_fields =["username","email"]
-    ordering =["id"]
-
-admin.site.register(User,CustomUserModel)
--- Event_Management_System/users/views.py
+++ Event_Management_System/users/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import User
+from django.http import JsonResponse
+import json
 
 # Create your views here.
+@csrf_exempt
+def register_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        username = data.get('username')
+        phone_number = data.get('phone_number')
+        email = data.get('email')
+        password = data.get('password')
+
+        user = User.objects.create_user(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+            phone_number = phone_number,
+            email = email,
+            password=password
+        )
+
+        return JsonResponse({
+            "message" : "User account was successfully created",
+            "user_id" : user.id
+        })
